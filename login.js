40ed9d5a1b4742043a3d9cf57d3ef6f3c8fc3439
02psg <princesingh21999@gmail.com>
2024-02-24 15:48:05 +0530

    commit
    Made With Love

---
+++ login.js
@@ -0,0 +1,16 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAXUdAGGJpUgXWv8UB0X-wOD6pYmGIwH0s",
+    authDomain: "login-form-5edd0.firebaseapp.com",
+    projectId: "login-form-5edd0",
+    storageBucket: "login-form-5edd0.appspot.com",
+    messagingSenderId: "716237140619",
+    appId: "1:716237140619:web:b99ac50a8a57877bf11a8e"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const auth= firebase.auth()
+  const database=firebase.database()
+  function register(){
+    
+  }
